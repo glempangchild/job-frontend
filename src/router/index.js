e892a990c925dba80532e8da2ef705aedbfa33d5
Dedy Setiawan <glempangchild@gmail.com>
2021-03-18 15:18:48 +0700

    menambahkan fitur login dan menyimpan token ke local storage

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import DataPasien from "../views/DataPasien.vue";
 import Dokter from "../views/Dokter.vue";
+import Login from "../views/Login.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -10,6 +11,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
   },
   {
     path: "/pasien",
